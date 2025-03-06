3272544e7f2f205912cc223c714a565c1f990064
lgammon <122504090+lgammon@users.noreply.github.com>
2025-03-06 14:12:34 -0800

    Update KelpDetection.js
    added classifier

--- KelpDetection.js
+++ KelpDetection.js
@@ -1,5 +1,8 @@
+
+
 var AOI = 
-    /* color: #B00B1E */
+    /* color: #b00b1e */
+    /* shown: false */
     /* displayProperties: [
       {
         "type": "rectangle"
@@ -11,8 +14,52 @@ var AOI =
           [-121.35579179298949, 35.91853281686468],
           [-121.35579179298949, 36.40194656170591]]], null, false),
     imageCollection = ee.ImageCollection("COPERNICUS/S2_HARMONIZED"),
-    cloudscore = ee.ImageCollection("GOOGLE/CLOUD_SCORE_PLUS/V1/S2_HARMONIZED");
-
+    cloudscore = ee.ImageCollection("GOOGLE/CLOUD_SCORE_PLUS/V1/S2_HARMONIZED"),
+    water = /* color: #d63000 */ee.FeatureCollection(
+        [ee.Feature(
+            ee.Geometry.Point([-121.72477844347537, 36.15100992166214]),
+            {
+              "landcover": 0,
+              "system:index": "0"
+            }),
+        ee.Feature(
+            ee.Geometry.Point([-121.68787124743045, 36.16043487298336]),
+            {
+              "landcover": 0,
+              "system:index": "1"
+            })]),
+    kelp = /* color: #98ff00 */ee.FeatureCollection(
+        [ee.Feature(
+            ee.Geometry.Point([-121.68718460192264, 36.16736426147979]),
+            {
+              "landcover": 1,
+              "system:index": "0"
+            }),
+        ee.Feature(
+            ee.Geometry.Point([-121.68649795641483, 36.16722567971289]),
+            {
+              "landcover": 1,
+              "system:index": "1"
+            }),
+        ee.Feature(
+            ee.Geometry.Point([-121.69314983477176, 36.17089801374621]),
+            {
+              "landcover": 1,
+              "system:index": "2"
+            })]),
+    forest = /* color: #0b4a8b */ee.FeatureCollection(
+        [ee.Feature(
+            ee.Geometry.Point([-121.681305199762, 36.171486956966504]),
+            {
+              "landcover": 2,
+              "system:index": "0"
+            }),
+        ee.Feature(
+            ee.Geometry.Point([-121.6774080928715, 36.172759415205114]),
+            {
+              "landcover": 2,
+              "system:index": "1"
+            })]);
 // Function to process Sentinel-2 images and compute histograms
 function processSentinel2(year, region, visualize) {
     // Define the date range for each year
@@ -95,6 +142,8 @@ function createMultiYearComposite(years, region) {
 
     var multiYearComposite = ee.ImageCollection(composites).median();
     
+    
+    
     // Compute and add NDVI, FAI, and KD only for the multi-year composite
     var NDVI = multiYearComposite.expression(
         "(NIR - RED) / (NIR + RED)",
@@ -126,55 +175,6 @@ function createMultiYearComposite(years, region) {
 
     return multiYearComposite;
 }
-
-//Merge into one FeatureCollection and print details to consloe
-var classNames = water.merge(kelp).merge(forest);
-print(classNames);
-
-//Extract training data from select bands of the image, print to console
-var bands = ['B2', 'B3', 'B4', 'B5', 'B6', 'B7'];
-var training = image.select(bands).sampleRegions({
-  collection: classNames,
-  properties: ['landcover'],
-  scale: 30
-});
-print(training);
-
-//Train classifier - e.g. cart, randomForest, svm
-var classifier = ee.Classifier.libsvm().train({
-  features: training,
-  classProperty: 'landcover',
-  inputProperties: bands
-});
-
-//Run the classification
-var classified = image.select(bands).classify(classifier);
-
-//Centre the map on your training data coverage
-//Map.centerObject(classNames, 11);
-//Add the classification to the map view, specify colours for classes
-Map.addLayer(classified,
-{min: 0, max: 1, palette: ['blue', 'red']},
-'classification');
-
-var classNames = water.merge(kelp).merge(forest);
-
-var validation = classified.sampleRegions({
-  collection: valNames,
-  properties: ['landcover'],
-  scale: 30,
-});
-print(validation);
-
-//Compare the landcover of your validation data against the classification result
-var testAccuracy = validation.errorMatrix('landcover', 'classification');
-//Print the error matrix to the console
-print('Validation error matrix: ', testAccuracy);
-//Print the overall accuracy to the console
-print('Validation overall accuracy: ', testAccuracy.accuracy());
-
-
-
 // Set the area of interest
 Map.centerObject(AOI,11);
 
@@ -190,3 +190,49 @@ processSentinel2(2020, AOI, true);
 
 // Example: Call the function for a multi-year median composite (2019-2021)
 var multiYearComposite = createMultiYearComposite([2019, 2020, 2021], AOI);
+
+//Merge into one FeatureCollection and print details to consloe
+var classNames = water.merge(kelp).merge(forest);
+print(classNames);
+
+//Extract training data from select bands of the image, print to console
+var bands = ['B2', 'B3', 'B4', 'B5', 'B6', 'B7'];
+var training = multiYearComposite.select(bands).sampleRegions({
+  collection: classNames,
+  properties: ['landcover'],
+  scale: 30
+});
+print(training);
+
+//Train classifier - e.g. cart, randomForest, svm
+var classifier = ee.Classifier.libsvm().train({
+  features: training,
+  classProperty: 'landcover',
+  inputProperties: bands
+});
+
+//Run the classification
+var classified = multiYearComposite.select(bands).classify(classifier);
+
+//Centre the map on your training data coverage
+//Map.centerObject(classNames, 11);
+//Add the classification to the map view, specify colours for classes
+Map.addLayer(classified,
+{min: 0, max: 1, palette: ['blue', 'red', 'green']},
+'classification');
+
+var valNames = water.merge(kelp).merge(forest);
+
+var validation = classified.sampleRegions({
+  collection: valNames,
+  properties: ['landcover'],
+  scale: 30,
+});
+print(validation);
+
+//Compare the landcover of your validation data against the classification result
+var testAccuracy = validation.errorMatrix('landcover', 'classification');
+//Print the error matrix to the console
+print('Validation error matrix: ', testAccuracy);
+//Print the overall accuracy to the console
+print('Validation overall accuracy: ', testAccuracy.accuracy());
